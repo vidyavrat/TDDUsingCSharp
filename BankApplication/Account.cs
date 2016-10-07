261d9fc936aa40c5043680555c4b394d33677850
vidyavrat <vidya_mct@yahoo.com>
2016-10-07 14:56:32 -0700

    TDD

---
+++ BankApplication/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication.Savings
+{
+    public class Account
+    {
+        private double accountBalance = 0.00;
+        private bool accountStatus = false;
+
+        private enum Roles
+        {
+            Customer,
+            Manager
+        }
+
+        public bool IsAccountActive(string accNumber)
+        {
+           return accountStatus;            
+        }
+
+        public double Balance(string accNumber)
+        {
+            return accountBalance;
+        }
+       
+        public double Deposit(string accNumber, double amount)
+        {
+            return accountBalance = accountBalance + amount;
+        }
+
+        public double Withdwral(string accNumber, double amount)
+        {
+            return accountBalance = accountBalance - amount;
+        }
+
+        private bool ActivateAccount(string accNumber, Roles userRole)
+        {
+            accountStatus = true;
+            return accountStatus;
+        }
+    }
+}
---
+++ UnitTestBankApplication/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankApplication.Savings;
+
+namespace UnitTestBankApplication
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Test if an account is active
+        [TestMethod][Priority(0)] [TestCategory("Happy Path")]
+        public void TestAccountStatus_Active_Success()
+        {            
+            // Arrange
+            Account objAccount = new Account();
+
+            // Act
+            bool actualResult = objAccount.IsAccountActive("1234");
+
+            //Assert
+            Assert.IsTrue(actualResult, "Failed; Account is not Active");
+        }
+        
+        //ToDo: Exception is thrown if Null is passed as an Account Number.
+        //[TestMethod] [Priority(0)] [TestCategory("Sad Path")]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void TestAccountStatus_ArgumentException_Success()
+        //{
+        //    // Arrange
+        //    Account objAccount = new Account();
+
+        //    // Act
+        //    bool actualResult = objAccount.IsAccountActive(null);
+        //}
+
+        //ToDo: Exception is thrown if WhiteSpace " " is passed as Account Number.
+        //[TestMethod] [Priority(0)] [TestCategory("Sad Path")]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void TestAccountStatus_AccountNumberWhiteSpace_ArgumentException_Success()
+        //{
+        //    // Arrange
+        //    Account objAccount = new Account();
+
+        //    // Act
+        //    bool actualResult = objAccount.IsAccountActive(" ");
+        //}  
+    }
+}
