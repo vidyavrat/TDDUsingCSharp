1433fc1f2d5bd942e3172257eb28b2259104952e
Vidya Vrat Agarwal <vidya_mct@yahoo.com>
2018-07-20 11:38:15 -0700

    Removed ToDo" from comments in UnitTest1.cs file.

--- UnitTestBankApplication/UnitTest1.cs
+++ UnitTestBankApplication/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using BankApplication.Savings;
 
 namespace UnitTestBankApplication
@@ -20,29 +21,33 @@ namespace UnitTestBankApplication
             //Assert
             Assert.IsTrue(actualResult, "Failed; Account is not Active");
         }
-        
-        //ToDo: Exception is thrown if Null is passed as an Account Number.
-        //[TestMethod] [Priority(0)] [TestCategory("Sad Path")]
-        //[ExpectedException(typeof(ArgumentException))]
-        //public void TestAccountStatus_ArgumentException_Success()
-        //{
-        //    // Arrange
-        //    Account objAccount = new Account();
 
-        //    // Act
-        //    bool actualResult = objAccount.IsAccountActive(null);
-        //}
+        // Exception is thrown if Null is passed as an Account Number.
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("Sad Path")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAccountStatus_ArgumentException_Success()
+        {
+            // Arrange
+            Account objAccount = new Account();
 
-        //ToDo: Exception is thrown if WhiteSpace " " is passed as Account Number.
-        //[TestMethod] [Priority(0)] [TestCategory("Sad Path")]
-        //[ExpectedException(typeof(ArgumentException))]
-        //public void TestAccountStatus_AccountNumberWhiteSpace_ArgumentException_Success()
-        //{
-        //    // Arrange
-        //    Account objAccount = new Account();
+            // Act
+            bool actualResult = objAccount.IsAccountActive(null);
+        }
 
-        //    // Act
-        //    bool actualResult = objAccount.IsAccountActive(" ");
-        //}  
+        // Exception is thrown if WhiteSpace " " is passed as Account Number.
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("Sad Path")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAccountStatus_AccountNumberWhiteSpace_ArgumentException_Success()
+        {
+            // Arrange
+            Account objAccount = new Account();
+
+            // Act
+            bool actualResult = objAccount.IsAccountActive(" ");
+        }
     }
 }
